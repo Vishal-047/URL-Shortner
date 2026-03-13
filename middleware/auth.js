2be7f6d9b36a6cda4bc4ebf26156d33f75abe7db
Vishal-047 <kingrw007@gmail.com>
2026-03-13 19:13:16 +0530

    update

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,30 +1,26 @@
 const { getUser } = require("../service/auth");
 
-async function restrictUser(req,res,next) {
-    const userUid=req.cookies.uid;
-    if(!userUid){
-        return res.redirect("/login");
+function restrictUser(req, res, next) {
+    const userUid = req.cookies.uid;
+    if (!userUid) {
+        return res.redirect(302, "/login");
     }
-    const user=getUser(userUid);
-
-    if(!user){
-        return res.redirect("/login");
+    const user = getUser(userUid);
+    if (!user) {
+        res.clearCookie("uid");                 // clean up invalid/expired token
+        return res.redirect(302, "/login");
     }
-
-    req.user=user;
-    next();
-
+    req.user = user;
+    return next();
 }
 
-async function checkAuth(req,res,next) {
-    const userUid=req.cookies.uid;
-    if(userUid){
-        const user=getUser(userUid);
-        req.user=user;
+function checkAuth(req, res, next) {
+    const userUid = req.cookies.uid;
+    if (userUid) {
+        const user = getUser(userUid);
+        req.user = user || null;               // don't attach null user silently
     }
-    next();
-    
+    return next();
 }
-module.exports={
-    restrictUser,checkAuth
-}
+
+module.exports = { restrictUser, checkAuth };
